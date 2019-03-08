0b33917d4604cb6f336c7925d1e91a8c911638dc
secondarykey <secondarykey@gmail.com>
2019-03-08 22:02:39 +0900

    chore : developers release tool
    リリース時のバックアップ等の効率化

--- dev_release.sh
+++ dev_release.sh
@@ -1,22 +1,37 @@
 
 echo "This shell is for development."
 echo "It deletes the file necessary for the operation from the .speaks directory."
-echo "still run it?[Y/n]"
+echo "Still run it?[Y/n]"
 
 read answer
 
 case $answer in
   Y)
-    tar cvf backup.tar .speaks
-    echo "move .speaks"
+
+    echo "Backup .speaks"
+    tar cvf - .speaks | gzip - > backup_`date '+%Y%m%d%H%M%S'`.tar.gz
+
+    echo "Move .speaks"
     cd .speaks
-    echo "remove speaks.ini"
+
+    echo "Remove speaks.ini"
     rm speaks.ini
-    echo "remove db file"
+
+    echo "Remove db file"
     rm *.db
-    echo "generate binary.go"
-    go-bindata.exe -pkg config -o ../config/binary.go ./...
-    echo "success."
+
+    case $? in
+      0)
+        echo "generate binary.go"
+        go-bindata.exe -pkg config -o ../config/binary.go ./...
+        echo "Success!"
+      ;;
+      1)
+        echo "DB remove error"
+      ;;
+    esac
+
     cd ..
+
     ;;
 esac
